42173008b70a6668cf43aac1281c6dd01bdce05d
Admin <scadmin@clarington.net>
2021-02-09 14:24:17 -0500

    Adding CommandSet to project.

---
+++ src/extensions/arInvoiceListCommandSet/loc/myStrings.d.ts
@@ -0,0 +1,9 @@
+declare interface IArInvoiceListCommandSetCommandSetStrings {
+  Command1: string;
+  Command2: string;
+}
+
+declare module 'ArInvoiceListCommandSetCommandSetStrings' {
+  const strings: IArInvoiceListCommandSetCommandSetStrings;
+  export = strings;
+}
