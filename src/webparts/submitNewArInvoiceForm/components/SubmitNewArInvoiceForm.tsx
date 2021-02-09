4618ac228a3081b4551d0f51d9e4bf17a8d77ef5
Admin <scadmin@clarington.net>
2021-02-09 15:09:31 -0500

    Building Kendo Form.

--- src/webparts/submitNewArInvoiceForm/components/SubmitNewArInvoiceForm.tsx
+++ src/webparts/submitNewArInvoiceForm/components/SubmitNewArInvoiceForm.tsx
@@ -5,24 +5,50 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 import { Form, Field, FormElement } from '@progress/kendo-react-form';
 import { Button } from '@progress/kendo-react-buttons';
+import { TextField, MaskedTextField } from 'office-ui-fabric-react/lib/TextField';
+
 
 export default class SubmitNewArInvoiceForm extends React.Component<ISubmitNewArInvoiceFormProps, {}> {
   public render(): React.ReactElement<ISubmitNewArInvoiceFormProps> {
+
+    const handleSubmit = (dataItem) => alert(JSON.stringify(dataItem, null, 2));
+
     return (
-      <div className={ styles.submitNewArInvoiceForm }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Form
+        onSubmit={handleSubmit}
+        render={(formRenderProps) => (
+          <FormElement style={{ width: 400 }}>
+            <fieldset className={'k-form-fieldset'}>
+              <legend className={'k-form-legend'}>Create New AR Invoice</legend>
+              <Field
+                id={'Title'}
+                name={'Title'}
+                label={'Title'}
+                component={TextField}
+              // validator={nameValidator}
+              />
+              <Field
+                id={'Invoice_x0020_Number'}
+                name={'Invoice_x0020_Number'}
+                label={'Invoice Number'}
+                // mask={'(999) 000-00-00-00'}
+                // hint={'Hint: Your active phone number.'}
+                component={TextField}
+              // validator={phoneValidator}
+              />
+
+              <div className="k-form-buttons">
+                <Button
+                  primary={true}
+                  type={'submit'}
+                  disabled={!formRenderProps.allowSubmit}
+                >Submit AR Invoice</Button>
+                <Button onClick={formRenderProps.onFormReset}>Clear</Button>
+              </div>
+            </fieldset>
+          </FormElement>
+        )}
+      />
     );
   }
 }
