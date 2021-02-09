208ef95439a171038ee1b32bd37588d10c241867
Admin <scadmin@clarington.net>
2021-02-09 14:13:29 -0500

    Adding first WebPart to project.

---
+++ src/webparts/submitNewArInvoiceForm/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISubmitNewArInvoiceFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SubmitNewArInvoiceFormWebPartStrings' {
+  const strings: ISubmitNewArInvoiceFormWebPartStrings;
+  export = strings;
+}
