e9fb4f64f07347bd31960aa88a6e66c9af34a022
Admin <scadmin@clarington.net>
2021-02-11 10:16:09 -0500

    Now fetching current user.

--- src/webparts/submitNewArInvoiceForm/components/SubmitNewArInvoiceForm.tsx
+++ src/webparts/submitNewArInvoiceForm/components/SubmitNewArInvoiceForm.tsx
@@ -16,7 +16,7 @@ import { Button } from '@progress/kendo-react-buttons';
 import { filterBy } from '@progress/kendo-data-query';
 
 // My Custom Imports
-import { GetUsersByLoginName } from '../../../MyHelperMethods/UserProfileMethods';
+import { GetUserProfileProperties, GetUsersByLoginName } from '../../../MyHelperMethods/UserProfileMethods';
 import { MyLists } from '../../../enums/MyLists';
 import * as MyFormComponents from '../../../components/MyFormComponents';
 import { GetChoiceFieldValues } from '../../../MyHelperMethods/HelperMethods';
@@ -37,6 +37,13 @@ export interface ISubmitNewArInvoiceFormState {
 
   // These are ALL the customers that we've queried from SharePoint. 
   allCustomerList?: any[];
+
+  // Determines if the Misc Customer fields should be rendered or not. 
+  // TRUE = Show Drop Down/ Combo Box. 
+  // FALSE = Show Misc Customer fields. 
+  showCustomerDropDown: boolean;
+
+  currentUser?: any;
 }
 //#endregion
 
@@ -47,8 +54,14 @@ export default class SubmitNewArInvoiceForm extends React.Component<ISubmitNewAr
     this.state = {
       departments: undefined,
       standardTerms: undefined,
-      displayCustomerList: undefined
+      displayCustomerList: undefined,
+      currentUser: undefined,
+      showCustomerDropDown: true
     };
+
+    sp.web.currentUser.get().then(user => {
+      GetUserProfileProperties(user.LoginName, values => this.setState({ currentUser: values }));
+    });
 
     GetChoiceFieldValues(MyLists["AR Invoice Requests"], 'Department').then(values => {
       this.setState({ departments: values });
@@ -88,182 +101,212 @@ export default class SubmitNewArInvoiceForm extends React.Component<ISubmitNewAr
   public render(): React.ReactElement<ISubmitNewArInvoiceFormProps> {
 
     const handleSubmit = (dataItem) => {
+      console.log(dataItem);
       alert(JSON.stringify(dataItem, null, 2));
-      sp.web.lists.getByTitle(MyLists['AR Invoice Requests']).items.add(dataItem).then(value => {
-        alert('It worked!');
-        this.props.submitCallback && this.props.submitCallback();
-      });
+      // sp.web.lists.getByTitle(MyLists['AR Invoice Requests']).items.add(dataItem).then(value => {
+      //   alert('It worked!');
+      //   this.props.submitCallback && this.props.submitCallback();
+      // });
     };
 
     return (
-      <Form
-        initialValues={{
-          Date: new Date(),
-          Urgent: false,
-          Standard_x0020_Terms: 'NET 30, 1% INTEREST CHARGED',
-          // GLAccounts: [],
-          // Department: this.state.currentUser && this.state.currentUser.Props['SPS-Department'],     
-        }}
-        onSubmit={handleSubmit}
-        render={(formRenderProps) => (
-          <FormElement style={{ maxWidth: '1200px', marginRight: 'auto', marginLeft: 'auto', padding: '15px' }}>
-            <fieldset className={'k-form-fieldset'}>
-              <b><legend className={'k-form-legend'}>Create New AR Invoice</legend></b>
+      <div>
+        {
+          this.state.currentUser &&
+          <Form
+            initialValues={{
+              Date: new Date(),
+              Urgent: false,
+              Standard_x0020_Terms: 'NET 30, 1% INTEREST CHARGED',
+              // GLAccounts: [],
+              // Department: this.state.currentUser && this.state.currentUser.Props['SPS-Department'],     
+            }}
+            onSubmit={handleSubmit}
+            render={(formRenderProps) => (
+              <FormElement style={{ maxWidth: '1200px', marginRight: 'auto', marginLeft: 'auto', padding: '15px' }}>
+                <fieldset className={'k-form-fieldset'}>
+                  <b><legend className={'k-form-legend'}>Create New AR Invoice</legend></b>
 
-              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <FieldWrapper>
-                  <Field
-                    id="Requested_x0020_By"
-                    name="Requested_x0020_By"
-                    label="Requested By"
-                    wrapperStyle={{ width: '100%' }}
-                    context={this.props.context}
-                    userEmail={this.props.context.pageContext.user.email}
-                    component={MyFormComponents.FormPersonaDisplay}
-                  />
-                </FieldWrapper>
-                <Field
-                  id={'Date'}
-                  name={'Date'}
-                  label={'* Date'}
-                  component={MyFormComponents.FormDatePicker}
-                  //validator={MyValidators.dateValidator}
-                  wrapperStyle={{ width: '50%' }}
-                />
-              </div>
+                  <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <FieldWrapper>
+                      <Field
+                        id="Requested_x0020_By"
+                        name="Requested_x0020_By"
+                        label="Requested By"
+                        wrapperStyle={{ width: '100%' }}
+                        context={this.props.context}
+                        userEmail={this.props.context.pageContext.user.email}
+                        component={MyFormComponents.FormPersonaDisplay}
+                      />
+                    </FieldWrapper>
+                    <Field
+                      id={'Date'}
+                      name={'Date'}
+                      label={'* Date'}
+                      component={MyFormComponents.FormDatePicker}
+                      //validator={MyValidators.dateValidator}
+                      wrapperStyle={{ width: '50%' }}
+                    />
+                  </div>
 
-              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Field
-                  id="Department"
-                  name="Department"
-                  label="* Department"
-                  wrapperStyle={{ width: '45%' }}
-                  data={this.state.departments ? this.state.departments : []}
-                  //validator={MyValidators.departmentValidator}
-                  component={MyFormComponents.FormDropDownList}
-                />
-                <Field
-                  id="Urgent"
-                  name="Urgent"
-                  label="Urgent"
-                  onLabel="Yes"
-                  offLabel="No"
-                  wrapperStyle={{ width: '50%' }}
-                  labelPlacement={'before'}
-                  component={MyFormComponents.FormCheckbox}
-                  hint={'Flag emails as high priority.'}
-                />
-              </div>
+                  <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Field
+                      id="Department"
+                      name="Department"
+                      label="* Department"
+                      wrapperStyle={{ width: '45%' }}
+                      data={this.state.departments ? this.state.departments : []}
+                      //validator={MyValidators.departmentValidator}
+                      component={MyFormComponents.FormDropDownList}
+                    />
+                    <Field
+                      id="Urgent"
+                      name="Urgent"
+                      label="Urgent"
+                      onLabel="Yes"
+                      offLabel="No"
+                      wrapperStyle={{ width: '50%' }}
+                      labelPlacement={'before'}
+                      component={MyFormComponents.FormCheckbox}
+                      hint={'Flag emails as high priority.'}
+                    />
+                  </div>
 
-              <FieldWrapper>
-                <Field
-                  id="Requires_x0020_Department_x0020_Id"
-                  name="Requires_x0020_Department_x0020_Id"
-                  label="* Requires Authorization By"
-                  wrapperStyle={{ width: '100%' }}
-                  dataItemKey="Email"
-                  textField="Title"
-                  hint={'Send an approval request to one or more users.'}
-                  //validator={MyValidators.requireOneOrMorePeople}
-                  personSelectionLimit={10}
-                  context={this.props.context}
-                  selectedItems={e => {
-                    if (e && e.length > 0) {
-                      GetUsersByLoginName(e).then(res => {
-                        /// Settings the user IDs here so that we can save them in the List item during the form submit event. 
-                        // formRenderProps.onChange('Requires_x0020_Department_x0020_Id', {
-                        //   value: { 'results': res.map(user => { return user.Id; }) }
-                        // });
+                  <FieldWrapper>
+                    <Field
+                      id="Requires_x0020_Department_x0020_Id"
+                      name="Requires_x0020_Department_x0020_Id"
+                      label="* Requires Authorization By"
+                      wrapperStyle={{ width: '100%' }}
+                      dataItemKey="Email"
+                      textField="Title"
+                      hint={'Send an approval request to one or more users.'}
+                      //validator={MyValidators.requireOneOrMorePeople}
+                      personSelectionLimit={10}
+                      context={this.props.context}
+                      selectedItems={e => {
+                        if (e && e.length > 0) {
+                          GetUsersByLoginName(e).then(res => {
+                            /// Settings the user IDs here so that we can save them in the List item during the form submit event. 
+                            // formRenderProps.onChange('Requires_x0020_Department_x0020_Id', {
+                            //   value: { 'results': res.map(user => { return user.Id; }) }
+                            // });
 
-                        // Setting this email here so it can be passed to a workflow when the form is submitted.
-                        // * By setting the users email here it saves us from querying this information during the forms submit event.  
-                        formRenderProps.onChange('Requires_x0020_Authorization_x0020_ByEmail', {
-                          value: { 'results': res.map(user => { return user.Email; }) }
-                        });
-                      });
-                    }
-                  }}
-                  component={MyFormComponents.FormPeoplePicker}
-                />
-              </FieldWrapper>
+                            // Setting this email here so it can be passed to a workflow when the form is submitted.
+                            // * By setting the users email here it saves us from querying this information during the forms submit event.  
+                            formRenderProps.onChange('Requires_x0020_Authorization_x0020_ByEmail', {
+                              value: { 'results': res.map(user => { return user.Email; }) }
+                            });
+                          });
+                        }
+                      }}
+                      component={MyFormComponents.FormPeoplePicker}
+                    />
+                  </FieldWrapper>
 
-              <FieldWrapper>
-                <Field
-                  id="Customer"
-                  name="Customer"
-                  label="* Customer"
-                  wrapperStyle={{ width: '100%' }}
-                  data={this.state.displayCustomerList}
-                  dataItemKey="Id"
-                  textField="Title"
-                  //validator={MyValidators.requiresCustomer}
-                  allowCustom={true}
-                  itemRender={this.customerItemRender}
-                  component={MyFormComponents.FormComboBox}
-                  filterable={true}
-                  suggest={true}
-                  onFilterChange={this.customerFilterChange}
-                />
-                {/* {
-                  this._ShowCustomerDetails(formRenderProps.valueGetter('Customer')) &&
-                  <Field
-                    id={'MiscCustomerDetails'}
-                    name={'MiscCustomerDetails'}
-                    label={'Enter Additional Customer Details'}
-                    placeholder={'Address, Postal Code, Contact, etc....'}
-                    component={MyFormComponents.FormTextArea}
-                  />
-                } */}
-              </FieldWrapper>
-
-              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Field
-                  id="Customer_x0020_PO_x0020_Number"
-                  name="Customer_x0020_PO_x0020_Number"
-                  label="Customer PO Number"
-                  //validator={MyValidators.requiresCustomerPONUmber}
-                  component={MyFormComponents.FormInput}
-                />
-
-                <Field
-                  id="Standard_x0020_Terms"
-                  name="Standard_x0020_Terms"
-                  label="Standard Terms"
-                  wrapperStyle={{ width: '50%' }}
-                  //defaultValue='NET 30, 1% INTEREST CHARGED'
-                  data={
-                    this.state.standardTerms
-                      ? this.state.standardTerms
-                      : []
+                  {
+                    this.state.showCustomerDropDown ?
+                      <FieldWrapper>
+                        <Field
+                          id="Customer"
+                          name="Customer"
+                          label="* Customer"
+                          wrapperStyle={{ width: '100%' }}
+                          data={this.state.displayCustomerList}
+                          dataItemKey="Id"
+                          textField="Title"
+                          //validator={MyValidators.requiresCustomer}
+                          allowCustom={false}
+                          itemRender={this.customerItemRender}
+                          component={MyFormComponents.FormComboBox}
+                          filterable={true}
+                          suggest={true}
+                          onFilterChange={this.customerFilterChange}
+                        />
+                        {/* {
+                    this._ShowCustomerDetails(formRenderProps.valueGetter('Customer')) &&
+                    <Field
+                      id={'MiscCustomerDetails'}
+                      name={'MiscCustomerDetails'}
+                      label={'Enter Additional Customer Details'}
+                      placeholder={'Address, Postal Code, Contact, etc....'}
+                      component={MyFormComponents.FormTextArea}
+                    />
+                  } */}
+                      </FieldWrapper> :
+                      <div>
+                        <FieldWrapper>
+                          <Field
+                            id="Customer_x0020_Name"
+                            name="Customer_x0020_Name"
+                            label="* Customer Name"
+                            //validator={MyValidators.requiresCustomerPONUmber}
+                            component={MyFormComponents.FormInput}
+                          />
+                        </FieldWrapper>
+                        <FieldWrapper>
+                          <Field
+                            id="Customer_x0020_Details"
+                            name="Customer_x0020_Details"
+                            label="* Customer Name"
+                            //validator={MyValidators.requiresCustomerPONUmber}
+                            component={MyFormComponents.FormTextArea}
+                          />
+                        </FieldWrapper>
+                      </div>
                   }
-                  component={MyFormComponents.FormDropDownList}
-                />
-              </div>
+                  <Button onClick={e => { e.preventDefault(); this.setState({ showCustomerDropDown: !this.state.showCustomerDropDown }); }} look='flat'>Click to {this.state.showCustomerDropDown ? 'manually enter customer details.' : 'search for customers.'}</Button>
 
-              <FieldWrapper>
-                <Field
-                  id="Details"
-                  name="Details"
-                  label="Invoice Details"
-                  component={MyFormComponents.FormTextArea}
-                />
-              </FieldWrapper>
+                  <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Field
+                      id="Customer_x0020_PO_x0020_Number"
+                      name="Customer_x0020_PO_x0020_Number"
+                      label="Customer PO Number"
+                      //validator={MyValidators.requiresCustomerPONUmber}
+                      component={MyFormComponents.FormInput}
+                    />
 
-              <p>TODO: Add Account Codes....</p>
-              <p>TODO: Add Attachment Upload....</p>
+                    <Field
+                      id="Standard_x0020_Terms"
+                      name="Standard_x0020_Terms"
+                      label="Standard Terms"
+                      wrapperStyle={{ width: '50%' }}
+                      //defaultValue='NET 30, 1% INTEREST CHARGED'
+                      data={
+                        this.state.standardTerms
+                          ? this.state.standardTerms
+                          : []
+                      }
+                      component={MyFormComponents.FormDropDownList}
+                    />
+                  </div>
 
-              <div className="k-form-buttons">
-                <Button
-                  primary={true}
-                  type={'submit'}
-                  disabled={!formRenderProps.allowSubmit}
-                >Submit AR Invoice</Button>
-                <Button onClick={formRenderProps.onFormReset}>Clear</Button>
-              </div>
-            </fieldset>
-          </FormElement>
-        )}
-      />
+                  <FieldWrapper>
+                    <Field
+                      id="Details"
+                      name="Details"
+                      label="Invoice Details"
+                      component={MyFormComponents.FormTextArea}
+                    />
+                  </FieldWrapper>
+
+                  <p>TODO: Add Account Codes....</p>
+                  <p>TODO: Add Attachment Upload....</p>
+
+                  <div className="k-form-buttons">
+                    <Button
+                      primary={true}
+                      type={'submit'}
+                      disabled={!formRenderProps.allowSubmit}
+                    >Submit AR Invoice</Button>
+                    <Button onClick={formRenderProps.onFormReset}>Clear</Button>
+                  </div>
+                </fieldset>
+              </FormElement>
+            )}
+          />
+        }
+      </div>
     );
   }
 }
