cc6473cbe5ecfef840a428e8da6f227b6b3feadb
Admin <scadmin@clarington.net>
2021-02-11 09:36:33 -0500

    Wrapping the form in a special kendo wrapper to fix the pop up bug.

--- src/extensions/arInvoiceListCommandSet/components/NewInvoiceSidePanel.tsx
+++ src/extensions/arInvoiceListCommandSet/components/NewInvoiceSidePanel.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+// MS & Fluent UI
 import { escape } from '@microsoft/sp-lodash-subset';
-
-import { Form, Field, FormElement } from '@progress/kendo-react-form';
-import { Button } from '@progress/kendo-react-buttons';
 import { TextField, MaskedTextField } from 'office-ui-fabric-react/lib/TextField';
 import { Panel, PanelType } from 'office-ui-fabric-react/lib/Panel';
-import SubmitNewArInvoiceForm from '../../../webparts/submitNewArInvoiceForm/components/SubmitNewArInvoiceForm';
 import { BaseDialog } from '@microsoft/sp-dialog';
 import { IFocusTrapZoneProps } from '@fluentui/react';
+
+// Kendo UI
+import { Form, Field, FormElement } from '@progress/kendo-react-form';
+import { Button } from '@progress/kendo-react-buttons';
+import { Popup, PopupPropsContext } from '@progress/kendo-react-popup';
+
+// My Custom
+import SubmitNewArInvoiceForm from '../../../webparts/submitNewArInvoiceForm/components/SubmitNewArInvoiceForm';
 
 
 export interface INewInvoiceSidePanelProps {
@@ -27,15 +32,21 @@ export default class NewInvoiceSidePanel extends React.Component<INewInvoiceSide
     }
 
     public render(): React.ReactElement<any> {
-        const fucusTrapZoneProps: IFocusTrapZoneProps = { disabled: true };
+        let wrapper = undefined;
         return (
             <Panel
                 isLightDismiss={false}
                 isOpen={this.state.isOpen}
                 type={this.props.panelType ? this.props.panelType : PanelType.medium}
-                focusTrapZoneProps={fucusTrapZoneProps}
             >
-                <SubmitNewArInvoiceForm context={this.props.context} submitCallback={() => { alert('call back worked too!'); this.setState({ isOpen: false }); }} />
+                <div ref={e => wrapper = e}>
+                    <PopupPropsContext.Provider value={props => ({ ...props, appendTo: wrapper })}>
+                        <SubmitNewArInvoiceForm
+                            context={this.props.context}
+                            submitCallback={() => { alert('call back worked too!'); this.setState({ isOpen: false }); }}
+                        />
+                    </PopupPropsContext.Provider>
+                </div>
             </Panel >
         );
     }
