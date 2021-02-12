e0cf7f05777e7b9814c12072cb843eaea48b9759
Admin <scadmin@clarington.net>
2021-02-12 10:10:02 -0500

    Making an account code component.

---
+++ src/webparts/submitNewArInvoiceForm/components/AccountCodeListComponent.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import { ListView, ListViewHeader } from '@progress/kendo-react-listview';
+import { Card, CardTitle, CardSubtitle, CardBody } from '@progress/kendo-react-layout';
+
+class AccountCodeListComponent extends Component {
+    render() {
+        return (
+            <div>
+                hello world. 
+            </div>
+        );
+    }
+}
+
+export default AccountCodeListComponent;
