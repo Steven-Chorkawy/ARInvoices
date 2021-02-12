cdc6b6a4e98f75534ea08ab425d06baaea3d6c2e
Admin <scadmin@clarington.net>
2021-02-12 12:10:57 -0500

    Adding a header button and custom list item component to the account list view component.

--- src/webparts/submitNewArInvoiceForm/components/AccountCodeListComponent.tsx
+++ src/webparts/submitNewArInvoiceForm/components/AccountCodeListComponent.tsx
@@ -2,13 +2,37 @@ import * as React from 'react';
 
 import { ListView, ListViewHeader } from '@progress/kendo-react-listview';
 import { Card, CardTitle, CardSubtitle, CardBody } from '@progress/kendo-react-layout';
+import { Button } from '@progress/kendo-react-buttons';
+
+//#region Component Methods
+const MyHeader = () => {
+    return (
+        <ListViewHeader style={{ color: 'rgb(160, 160, 160)', fontSize: 14 }} className='pl-3 pb-2 pt-2'>
+            <Button look='flat' icon='plus' primary={true} onClick={e => { e.preventDefault(); }}>Add New Account</Button>
+        </ListViewHeader>
+    )
+}
+//#endregion
+
+class AccountCodeItem extends React.Component {
+    render() {
+        return (<div>hello world</div>);
+    }
+}
 
 export class AccountCodeListComponent extends React.Component {
+
+    //AccountCodeItem = props => <AccountCodeItem {...props} saveItem={this.saveData} deleteItem={this.deleteItem} />
+    AccountCodeItem = props => <AccountCodeItem {...props} />
+
     render() {
         return (
-            <div>
-                hello world. 
-            </div>
+            <ListView
+                // data={this.state.data}
+                item={this.AccountCodeItem}
+                // style={{ width: "100%", height: 500 }}
+                header={MyHeader}
+            />
         );
     }
 }
