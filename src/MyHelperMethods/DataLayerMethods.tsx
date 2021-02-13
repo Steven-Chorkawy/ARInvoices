8906a5daff7b649a8e83c312f62c27055b88cce9
Steven Chorkawy <schorkawy@clarington.net>
2021-02-13 12:02:37 -0500

    Catching any errors if the Create AR Invoice method fails.

--- src/MyHelperMethods/DataLayerMethods.tsx
+++ src/MyHelperMethods/DataLayerMethods.tsx
@@ -11,10 +11,13 @@ import { IARInvoice, ISaveARInvoice } from '../interfaces/IARInvoice';
 
 // TODO: replace invoice: any with an invoice interface. 
 export const CreateARInvoice = async (invoice: any) => {
-    const { AccountCodes, Attachments, Customer } = invoice;
+    const { AccountCodes, Attachments, Customer, ApproverEmails } = invoice;
     debugger;
 
     sp.web.lists.getByTitle(MyLists['AR Invoice Requests']).items.add(invoice).then(value => {
         alert('It from CreateARInvoice!');
+    }).catch(error => {
+        console.log(error);
+        alert('CreateARInvoice failed....');
     });
 };
