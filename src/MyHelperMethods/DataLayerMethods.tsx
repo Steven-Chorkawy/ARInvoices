06097dccc128ad58bc8c373c5722d460ce5fd774
Admin <scadmin@clarington.net>
2021-02-17 11:24:14 -0500

    Now uploading attachments.

--- src/MyHelperMethods/DataLayerMethods.tsx
+++ src/MyHelperMethods/DataLayerMethods.tsx
@@ -1,36 +1,35 @@
 import { sp } from "@pnp/sp";
+import { IAttachmentInfo } from "@pnp/sp/attachments";
+import { IItem } from "@pnp/sp/items/types"
 import "@pnp/sp/webs";
 import "@pnp/sp/files";
 import "@pnp/sp/fields";
 import "@pnp/sp/folders";
 import "@pnp/sp/lists";
 import "@pnp/sp/items";
+import "@pnp/sp/attachments";
 
 import { MyLists } from '../enums/MyLists';
 import { IARInvoice, ISaveARInvoice } from '../interfaces/IARInvoice';
 
-export const UploadARInvoiceAttachment = async (attachment: any, arInvoiceId: number) => {
-    debugger;
-    let item = await sp.web.lists.getByTitle(MyLists["AR Invoice Requests"]).items.getById(arInvoiceId).get();
-    // TODO: What happens when this item is not found? 
-
-    item.attachmentFiles.add("file2.txt", "Here is my content").then(v => {
-        debugger;
-        console.log(v);
-    });
+export const UploadARInvoiceAttachments = async (attachments: any[], arInvoiceId: number) => {
+    let item = await sp.web.lists.getByTitle(MyLists["AR Invoice Requests"]).items.getById(arInvoiceId);
+    let uploadResults = [];
+    for (let attachmentIndex = 0; attachmentIndex < attachments.length; attachmentIndex++) {
+        const attachment = attachments[attachmentIndex];
+        uploadResults.push(await item.attachmentFiles.add(attachment.name, attachment.getRawFile()));
+    }
+    return uploadResults;
 }
-
 
 export const CreateARInvoice = async (data: any) => {
     const { AccountCodes, Attachments, Customer, ApproverEmails, Invoice } = data;
 
-    sp.web.lists.getByTitle(MyLists['AR Invoice Requests']).items.add(Invoice).then(value => {
-        alert('It from CreateARInvoice!');
-        return 'it has been done!';
-    }).catch(error => {
-        console.log(error);
-        alert('CreateARInvoice failed....');
-        return 'it has been done!';
-    });
+    let itemAddResult = await sp.web.lists.getByTitle(MyLists['AR Invoice Requests']).items.add(Invoice);
+    let newARInvoice = (await itemAddResult).data;
+
+    let uploadResult = await UploadARInvoiceAttachments(Attachments, newARInvoice.ID);
+
+    debugger;
 };
 
