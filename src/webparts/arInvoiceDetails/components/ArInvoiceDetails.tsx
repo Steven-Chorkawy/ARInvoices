a1f9b134193b3debbcb10a82095efcf9efd00338
Admin <scadmin@clarington.net>
2021-02-19 15:01:19 -0500

    Adding props to class.

--- src/webparts/arInvoiceDetails/components/ArInvoiceDetails.tsx
+++ src/webparts/arInvoiceDetails/components/ArInvoiceDetails.tsx
@@ -4,7 +4,12 @@ export interface IArInvoiceDetailsProps {
   description: string;
 }
 
-export default class ArInvoiceDetails extends React.Component<IArInvoiceDetailsProps, {}> {
+export default class ArInvoiceDetails extends React.Component<IArInvoiceDetailsProps, any> {
+  
+  constructor(props) {
+    super(props);    
+  }
+
   public render(): React.ReactElement<IArInvoiceDetailsProps> {
     return (
       <h1>hello world</h1>
