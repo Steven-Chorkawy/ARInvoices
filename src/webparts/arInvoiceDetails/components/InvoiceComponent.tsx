5e081815a45319703c5020feb375f814bc385e2f
Admin <scadmin@clarington.net>
2021-02-22 14:09:05 -0500

    Displaying customer details.

--- src/webparts/arInvoiceDetails/components/InvoiceComponent.tsx
+++ src/webparts/arInvoiceDetails/components/InvoiceComponent.tsx
@@ -28,8 +28,25 @@ export class InvoiceComponent extends React.Component<IArInvoiceSubComponentProp
             <Card style={{ width: '100%' }}>
                 <CardBody>
                     <CardTitle><b>Customer Details</b></CardTitle>
-                    <Label>Name:</Label>
 
+                    <div className='row'>
+                        <div className='col-md-6'>
+                            <Label>Name:</Label>
+                            {
+                                this.props.invoice.Customer ?
+                                    <p>{this.props.invoice.Customer.Title}</p> :
+                                    <p>{this.props.invoice.Customer_x0020_Name}</p>
+                            }
+                        </div>
+                        <div className='col-md-6'>
+                            <Label>Details:</Label>
+                            {
+                                this.props.invoice.Customer ?
+                                    <p>{this.props.invoice.Customer.Mailing_x0020_Address}</p> :
+                                    <p>{this.props.invoice.Customer_x0020_Details}</p>
+                            }
+                        </div>
+                    </div>
                 </CardBody>
             </Card>
         );
