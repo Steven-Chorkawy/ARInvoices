4c0cfb92c2425697f3eadaa698a6cebbf86ec539
Admin <scadmin@clarington.net>
2021-02-22 15:38:21 -0500

    Making a method to build a URL to the documents.

--- src/MyHelperMethods/HelperMethods.tsx
+++ src/MyHelperMethods/HelperMethods.tsx
@@ -13,3 +13,13 @@ export const GetChoiceFieldValues = async (listName: MyLists, internalNameOrTitl
     let field: any = await sp.web.lists.getByTitle(listName).fields.getByInternalNameOrTitle(internalNameOrTitle).select('Choices').get();
     return field.Choices;
 };
+
+/**
+ * Build a URL to access a document. 
+ * @param documentTitle Title of the document.
+ * @param documentLibrary Name of the Document Library.  Default MyLists["Related Invoice Attachments"].
+ */
+export const BuildURLToDocument = async (documentTitle: string, invoiceID: number): Promise<string> => {
+    //https://claringtonnet.sharepoint.com/sites/ARTest2/Lists/AR%20Invoice%20Requests/Attachments/73/AR%20Retention%20Meeting%20Aug%2018.docx?web=1
+    return `${await (await sp.web.get()).Url}/Lists/${encodeURI(MyLists["AR Invoice Requests"])}/Attachments/${invoiceID}//${encodeURI(documentTitle)}?web=1`;
+};
