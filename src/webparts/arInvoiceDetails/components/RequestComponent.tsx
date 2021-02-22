ee597857c49176b57dcee497cf7293c32fb199d9
Admin <scadmin@clarington.net>
2021-02-22 12:17:43 -0500

    Request component is now rendering.

--- src/webparts/arInvoiceDetails/components/RequestComponent.tsx
+++ src/webparts/arInvoiceDetails/components/RequestComponent.tsx
@@ -11,21 +11,38 @@ import { filterBy } from '@progress/kendo-data-query';
 import { Form, Field, FormElement, FieldWrapper } from '@progress/kendo-react-form';
 import { Label, Error, Hint, FloatingLabel } from '@progress/kendo-react-labels';
 import { Button } from '@progress/kendo-react-buttons';
+import { Card, CardBody, CardTitle } from '@progress/kendo-react-layout';
+
+import Moment from 'react-moment';
+
+
+import { IArInvoiceSubComponentProps } from './ArInvoiceDetails';
+
+import { PersonaComponent } from '../../../components/PersonaComponent';
 
 /**
  * This class displays data about the request. 
  */
-export class RequestComponent extends React.Component<any, any> {
+export class RequestComponent extends React.Component<IArInvoiceSubComponentProps> {
     constructor(props) {
         super(props);
-
     }
 
     public render() {
         return (
-            <div>
-
-            </div>
+            <Card style={{ width: '100%' }}>
+                <CardBody>
+                    <CardTitle><b>Request Details</b></CardTitle>
+                    <Label>Requested By:</Label>
+                    <PersonaComponent userEmail={this.props.invoice.Requested_x0020_By.EMail} />
+                    <Label>Department:</Label>
+                    <p>{this.props.invoice.Department}</p>
+                    <Label>Date:</Label>
+                    <p>{<Moment format="D MMM YYYY">{this.props.invoice.Date}</Moment>}</p>
+                    <Label>Note:</Label>
+                    <p>{this.props.invoice.Details}</p>
+                </CardBody>
+            </Card>
         );
     }
 }
