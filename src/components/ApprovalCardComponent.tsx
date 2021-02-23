d3be0b8bbf106011244e0dc75b07772bb961b20f
Admin <scadmin@clarington.net>
2021-02-23 14:35:36 -0500

    Adding styles to the approval cards.

--- src/components/ApprovalCardComponent.tsx
+++ src/components/ApprovalCardComponent.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 
-import { Card, CardBody, CardSubtitle, CardTitle } from '@progress/kendo-react-layout';
+import { Card, CardActions, CardBody, CardSubtitle, CardTitle } from '@progress/kendo-react-layout';
 
 import { IARInvoice, IApproval } from '../interfaces/IARInvoice';
 import * as ApprovalEnum from '../enums/Approvals';
+import Moment from 'react-moment';
+import { Button } from '@progress/kendo-react-buttons';
 
-export interface IApprovalCardComponent {
+export interface IApprovalCardComponentProps {
     invoice: IARInvoice;
     approval: IApproval;
 };
 
+export interface IApprovalCardComponentState {
+    showMore: boolean;
+}
 const parseActionType = (action: IApproval) => {
     let output = 'k-i-info';
     switch (action.Request_x0020_Type) {
@@ -32,24 +37,84 @@ const parseActionType = (action: IApproval) => {
     return output;
 };
 
-export class ApprovalCardComponent extends React.Component<IApprovalCardComponent, any> {
+const parseActionStatus = (action: IApproval) => {
+    let output = '';
+    switch (action.Status) {
+        case ApprovalEnum.ApprovalStatus.Waiting:
+            output = 'k-state-info';
+            break;
+        case ApprovalEnum.ApprovalStatus.Approve:
+            output = 'k-state-success';
+            break;
+        case ApprovalEnum.ApprovalStatus.Cancel:
+        case ApprovalEnum.ApprovalStatus.Reject:
+            output = 'k-state-error';
+            break;
+        default:
+            output = '';
+            break;
+    }
+    return output;
+}
+
+export class ApprovalCardComponent extends React.Component<IApprovalCardComponentProps, IApprovalCardComponentState> {
     constructor(props) {
         super(props);
+        this.state = {
+            showMore: false
+        };
     }
-
-
 
     public render() {
         return (
-            <Card style={{ width: '100%' }}>
-                <CardBody>
+            <Card style={{ width: '100%', marginBottom: '5px' }}>
+                <CardBody className={parseActionStatus(this.props.approval)}>
                     <CardSubtitle>
                         <b title={this.props.approval.Status}><span className={`k-icon ${parseActionType(this.props.approval)}`}></span> | {this.props.approval.Request_x0020_Type}</b>
                     </CardSubtitle>
+                </CardBody>
+                <CardBody>
                     <div>
-                        {this.props.approval.Assigned_x0020_To.Title} | {this.props.approval.Status}
+                        {this.props.approval.Status === ApprovalEnum.ApprovalStatus.Waiting ? `Waiting for ` : `${this.props.approval.Status} by `}
+                        <b>{this.props.approval.Assigned_x0020_To.Title} </b>
+                        <Moment
+                            className={'k-card-subtitle'}
+                            date={this.props.approval.Modified}      // The date to be used.
+                            format={'MM/DD/YYYY'}       // Date format. 
+                            withTitle={true}            // Show Title on hover.
+                            titleFormat={'D MMM YYYY'}  // Title format
+                            fromNow={true}              // Display number of hours since date.
+                            fromNowDuring={7200000}    // Only display fromNow if it is less than the milliseconds provided here. 7200000 = 2 hours.
+                        />
                     </div>
                 </CardBody>
+                {
+                    this.state.showMore &&
+                    <CardBody style={{ wordWrap: 'break-word' }}>
+                        <div>
+                            Requested by <b>{this.props.approval.Author.Title} </b>
+                            <Moment
+                                className={'k-card-subtitle'}
+                                date={this.props.approval.Created}        // The date to be used.
+                                format={'MM/DD/YYYY'}       // Date format. 
+                                withTitle={true}            // Show Title on hover.
+                                titleFormat={'D MMM YYYY'}  // Title format
+                                fromNow={true}              // Display number of hours since date.
+                                fromNowDuring={7200000}     // Only display fromNow if it is less than the milliseconds provided here. 7200000 = 2 hours.
+                            />
+                        </div>
+                        <div>
+                            {this.props.approval.Notes}
+                        </div>
+                        <hr />
+                        <div>
+                            {this.props.approval.Response_x0020_Summary}
+                        </div>
+                    </CardBody>
+                }
+                <CardActions orientation='vertical'>
+                    <Button look='flat' onClick={() => this.setState({ showMore: !this.state.showMore })}>{this.state.showMore ? 'Hide' : 'Show More'}</Button>
+                </CardActions>
             </Card>
         );
     }
