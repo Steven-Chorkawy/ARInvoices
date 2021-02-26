e1223457f2e150f2136abb8f79dc0a7e604edd8b
Admin <scadmin@clarington.net>
2021-02-26 15:30:46 -0500

    Removing cancel and edit.

--- src/interfaces/IDataOperations.tsx
+++ src/interfaces/IDataOperations.tsx
@@ -2,7 +2,5 @@ export default interface IDataOperations {
     onAdd?: Function;       // Create new record in SharePoint.
     onRemove?: Function;    // Remove from UI, not from SharePoint.
     onSave?: Function;      // Update an existing item.
-    onCancel?: Function;    // Undo any changes made.
-    onEdit?: Function;      // Enter Edit mode.
     onDelete?: Function;    // Delete item from SharePoint.
 }
