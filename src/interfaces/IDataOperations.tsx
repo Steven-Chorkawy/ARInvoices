500cfe71749df3712bcad81bcd9498f15590d958
Admin <scadmin@clarington.net>
2021-02-26 14:17:01 -0500

    Adding comments.

--- src/interfaces/IDataOperations.tsx
+++ src/interfaces/IDataOperations.tsx
@@ -1,8 +1,8 @@
 export default interface IDataOperations {
-    onAdd?: Function;
-    onRemove?: Function;
-    onSave?: Function;
-    onCancel?: Function;
-    onEdit?: Function;
-    onDelete?: Function;
+    onAdd?: Function;       // Create new record in SharePoint.
+    onRemove?: Function;    // Remove from UI, not from SharePoint.
+    onSave?: Function;      // Update an existing item.
+    onCancel?: Function;    // Undo any changes made.
+    onEdit?: Function;      // Enter Edit mode.
+    onDelete?: Function;    // Delete item from SharePoint.
 }
