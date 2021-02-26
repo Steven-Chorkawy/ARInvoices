5856defd92997136db02ed3451b7c1ce9c5b2190
Admin <scadmin@clarington.net>
2021-02-26 12:10:13 -0500

    Making a new interface.

---
+++ src/interfaces/IDataOperations.tsx
@@ -0,0 +1,8 @@
+export default interface IDataOperations {
+    onAdd?: Function;
+    onRemove?: Function;
+    onSave?: Function;
+    onCancel?: Function;
+    onEdit?: Function;
+    onDelete?: Function;
+}
