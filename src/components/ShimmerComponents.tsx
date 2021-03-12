30175419d876005cf836cb4f0355f4dcdcf39926
Admin <scadmin@clarington.net>
2021-03-12 14:15:21 -0500

    Adding another shimmer component.

--- src/components/ShimmerComponents.tsx
+++ src/components/ShimmerComponents.tsx
@@ -2,30 +2,38 @@ import * as React from 'react';
 import { Shimmer, ShimmerElementsGroup, ShimmerElementType } from 'office-ui-fabric-react/lib/Shimmer';
 
 
-export class PersonaShimmer extends React.Component<any, any> {
+export class PersonaShimmer extends React.Component {
     public render() {
         const wrapperStyles = { display: 'flex' };
         const getCustomElements = (): JSX.Element => {
-            return (
-                <div style={wrapperStyles}>
-                    <ShimmerElementsGroup
-                        shimmerElements={[
-                            { type: ShimmerElementType.circle, height: 40 },
-                            { type: ShimmerElementType.gap, width: 16, height: 40 },
-                        ]}
-                    />
-                    <ShimmerElementsGroup
-                        flexWrap
-                        width="100%"
-                        shimmerElements={[
-                            { type: ShimmerElementType.line, width: '100%', height: 10, verticalAlign: 'bottom' },
-                            { type: ShimmerElementType.line, width: '90%', height: 8 },
-                            { type: ShimmerElementType.gap, width: '10%', height: 20 },
-                        ]}
-                    />
-                </div>
-            );
+            return <div style={wrapperStyles}>
+                <ShimmerElementsGroup
+                    shimmerElements={[
+                        { type: ShimmerElementType.circle, height: 40 },
+                        { type: ShimmerElementType.gap, width: 16, height: 40 },
+                    ]}
+                />
+                <ShimmerElementsGroup
+                    flexWrap
+                    width="100%"
+                    shimmerElements={[
+                        { type: ShimmerElementType.line, width: '100%', height: 10, verticalAlign: 'bottom' },
+                        { type: ShimmerElementType.line, width: '90%', height: 8 },
+                        { type: ShimmerElementType.gap, width: '10%', height: 20 },
+                    ]}
+                />
+            </div>;
         };
         return <Shimmer customElementsGroup={getCustomElements()} />;
+    }
+}
+
+export class RowShimmer extends React.Component {
+    public return() {
+        return <div>
+            <Shimmer />
+            <Shimmer width="75%" />
+            <Shimmer width="50%" />
+        </div>;
     }
 }
